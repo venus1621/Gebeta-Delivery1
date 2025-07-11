eb13c9385610106aee8a30047d61fe2ad672cae6
Venus1621 <venushailemeskel59@gmail.com>
2025-07-11 11:33:42 +0300

    Restaurnat to calculate by the current adress and display

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(helmet());
 
 // 🌐 Enable CORS (Update `origin` as needed)
 app.use(cors({
-  origin: 'https://kvhk98.csb.app', // 🔁 CodeSandbox frontend
+  origin: 'https://v0-backend-login-page.vercel.app', // 🔁 CodeSandbox frontend
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
