1cb757314ff96e14772ea22ed4b1f0923eae37d7
Venus1621 <venushailemeskel59@gmail.com>
2025-07-12 21:11:57 +0300

    Restaurnat to calculate by the current adress and display

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(cors());
 app.use(helmet());
 
 // 🌐 Enable CORS (Update `origin` as needed)
-
 // app.options('*', cors());
 
 // 🛡️ Rate limiting
@@ -32,7 +31,7 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
-app.use('/api', limiter);
+// app.use('/api', limiter);
 
 // 🧼 Body parser & sanitization
 app.use(express.json({ limit: '10kb' }));
